ed04c5964ef36187bbc1919dbaa076cf1f8c6d08
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-12-31 23:20:46 +0000

    Load help data from JSON file and update HelpView fields and context

--- help/views.py
+++ help/views.py
@@ -1,12 +1,22 @@
 """Help app view"""
+import json
+
 from django.views.generic.edit import CreateView
-from django.shortcuts import render
+# from django.shortcuts import render
 
 from .models import Help
 
-
+# Get help.json loaded as a constant
+with open("help.json", 'r', encoding="utf-8") as help_file:
+    HELP_DATA = json.load(help_file)
 class HelpView(CreateView):
-    template_name = 'help/help.html'
+    """The Help View"""
+    template_name = "help.html"
     model = Help
-    fields = ['name', 'email', 'message']
-    success_url = '/help/'
+    fields = ["channel", "role", "specific"]
+    success_url = "/help/"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["HELP_DATA"] = HELP_DATA
+        return context
