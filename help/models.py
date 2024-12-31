96d8a30acfce3f5211e499c4b5a0658c777dd8ec
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-12-31 22:49:33 +0000

    Add Help model with role validation and user association
    Role is helper/helped

--- help/models.py
+++ help/models.py
@@ -1,3 +1,18 @@
+"""The Help Model"""
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+HELP_ROLES = ("helper", "helped")
+
+# Validate, role can only be "helper" or "helped"
+def validate_role(value):
+    """Validate role"""
+    if value not in HELP_ROLES:
+        raise ValidationError("Role must be either 'helper' or 'helped'")
+
+class Help(models.Model):
+    """The Help Model"""
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    channel = models.CharField(max_length=100)
+    role = models.EmailField(validators=[validate_role])
+    specific = models.TextField()
