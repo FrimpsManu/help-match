28ebc827e0a75e62cdcecdcdd1b252323da1a53f
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2025-01-07 23:35:51 +0000

    Add Pod class to represent individual pod instances in the game grid

---
+++ static/game/pod.js
@@ -0,0 +1,11 @@
+import { Game } from "./game";
+
+class Pod{
+    constructor(row, col, rowParent){
+        this.row = row;
+        this.col = col;
+        (this.pod = rowParent.insertCell()).className = "pod";
+    }
+}
+
+export { Pod };
