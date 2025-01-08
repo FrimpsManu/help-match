3b535b4af75831cdb6c07b818069e1bc373a4896
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2025-01-08 00:38:01 +0000

    Add game view with login requirement and basic rendering

--- game/views.py
+++ game/views.py
@@ -1,3 +1,13 @@
+from django.views import View
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class Game(View, LoginRequiredMixin):
+    """The game view"""
+    def get(self, request):
+        """Render the game page"""
+        # person = request.user.person
+        # game_details = {
+        #     ""
+        # }
+        return render(request, "game.html")
