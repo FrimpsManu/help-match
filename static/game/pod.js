ab94a03f4b8fca54fcebdef0dc8fed6fdd5deaec
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2025-01-08 04:01:31 +0000

    Add Game import to Pod class and associate pod instance with its DOM element Also moved the get method from Game class to Pod class. Pod.get is more natural

--- static/game/pod.js
+++ static/game/pod.js
@@ -1,8 +1,15 @@
+import { Game } from "./game.js";
+
 class Pod{
     constructor(row, col, rowParent){
         this.row = row;
         this.col = col;
         (this.pod = rowParent.insertCell()).className = "pod";
+        this.pod.obj = this;
+    }
+
+    static get(row, col) {
+        return Game.site.rows[row].cells[col];
     }
 }
 
