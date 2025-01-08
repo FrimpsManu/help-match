efb6b65ab074bfe90023c6cecb48b039fcce429b
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2025-01-08 04:41:06 +0000

    Add simulated user data to game view for rendering game details

--- game/views.py
+++ game/views.py
@@ -4,10 +4,27 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class Game(View, LoginRequiredMixin):
     """The game view"""
+
+    person_fields = ("role", "channel", "specific")
+    user_fields = ("first_name", "last_name", "username")
+
     def get(self, request):
         """Render the game page"""
-        # person = request.user.person
-        # game_details = {
-        #     ""
-        # }
-        return render(request, "game.html")
+        # Do a simulated request.user
+        request.user = {
+            "first_name": "John",
+            "last_name": "Doe",
+            "username": "johndoe",
+            "person": {
+                "role": "helper",
+                "channel": "academic-math",
+                "specific": "Calculus"
+            }
+        }
+        user, person = request.user, request.user["person"]
+        game_data = {field: person[field] for field in self.person_fields}
+        game_data.update({field: user[field] for field in self.user_fields})
+        game_details = {
+            "game_data": game_data
+        }
+        return render(request, "game.html", game_details)
