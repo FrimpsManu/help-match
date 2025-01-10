073699bd00ef52cb7199bcc3ae49ade56ae55018
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2025-01-10 13:46:10 +0000

    Brought back role validation function to Person model to ensure it is either 'helper' or 'helped'. Not using it.

--- home/models.py
+++ home/models.py
@@ -1,11 +1,17 @@
 """The Person (to (be) help(ed)) Model"""
 from django.db import models
-from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
+HELP_ROLES = ("helper", "helped")
+
+# Validate, role can only be "helper" or "helped"
+def validate_role(value):
+    """Validate role"""
+    if value not in HELP_ROLES:
+        raise ValidationError("Role must be either 'helper' or 'helped'")
 
 class Person(models.Model):
-    """The Person Model"""
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    """The Help Model"""
     user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
     channel = models.CharField(max_length=100)
     role = models.EmailField()
