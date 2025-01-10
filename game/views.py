843721839c2ba68db69e0933a68ce66015a58883
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2025-01-10 15:11:57 +0000

    Add error messages for incomplete user data in game view

--- game/views.py
+++ game/views.py
@@ -1,5 +1,7 @@
+"""The game view module"""
 from django.views import View
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class Game(LoginRequiredMixin, View):
@@ -12,11 +14,14 @@ class Game(LoginRequiredMixin, View):
         """Render the game page"""
         user = request.user
         if not hasattr(user, "person"):
+            messages.error(request, "Please fill in all fields")
             return redirect("/")
         person = user.person
         game_data = {field: getattr(person, field) for field in self.person_fields}
         game_data.update({field: getattr(user, field) for field in self.user_fields})
         if not all(game_data.values()):
+            print(game_data)
+            messages.error(request, "Please fill in all fields")
             return redirect("/")
         game_details = {
             "game_data": game_data
