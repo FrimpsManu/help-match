9b20f2dab50df076d1c9a1ef9dd34b35ccb9a736
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2025-01-10 13:36:44 +0000

    Change user field from ForeignKey to OneToOneField in Person model

--- home/models.py
+++ home/models.py
@@ -12,7 +12,7 @@ def validate_role(value):
 
 class Person(models.Model):
     """The Help Model"""
-    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
     channel = models.CharField(max_length=100)
     role = models.EmailField(validators=[validate_role])
     specific = models.TextField()
